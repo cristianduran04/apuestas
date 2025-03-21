b9fa974f600d70836cfeb6d261701262e0a6cf38
cristianduran04 <cristian.alejo004@gmail.com>
2025-03-20 19:09:58 -0500

    0.2

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA6-yruvoPpWdlFpRYkFGF7UzSc22__Sdo",
   authDomain: "apuesta-c4281.firebaseapp.com",
   projectId: "apuesta-c4281",
-  storageBucket: "apuesta-c4281.appspot.com", // Corregido el dominio del storage
+  storageBucket: "apuesta-c4281.appspot.com",
   messagingSenderId: "1072201658218",
   appId: "1:1072201658218:web:cbd383b9c905454448bf13",
   measurementId: "G-1WWFJ45K2P"
@@ -13,7 +13,6 @@ const firebaseConfig = {
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // Inicializa Firestore
+const db = getFirestore(app);
 
-export { db };
-
+export { db, collection, getDocs, addDoc };
